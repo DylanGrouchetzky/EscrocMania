24a1cda7a89552b9630c9108d1048cb4a63637d5
DylanGrouchetzky <mrgrouchetzkydylan@gmail.com>
2023-09-04 12:37:52 +0200

    first commit

---
+++ src/Frontend/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Frontend\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('frontend/pages/index.html.twig');
+    }
+}
