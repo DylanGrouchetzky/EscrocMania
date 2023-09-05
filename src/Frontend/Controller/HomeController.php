1b6f51c28bb4c3aad3da058da7872917adc17826
DylanGrouchetzky <mrgrouchetzkydylan@gmail.com>
2023-09-05 12:55:13 +0200

    add page maquette

--- src/Frontend/Controller/HomeController.php
+++ src/Frontend/Controller/HomeController.php
@@ -9,8 +9,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('frontend/pages/index.html.twig');
+        return $this->render('frontend/pages/home.html.twig');
+    }
+
+    #[Route('/mentions-legales', name: 'mention_legales')]
+    public function mentionsLegale(): Response
+    {
+        return $this->render('frontend/pages/mentions_legales.html.twig');
+    }
+
+    #[Route('/politique-de-confidentialite', name: 'politique_de_confidentialite')]
+    public function politiqueDeConfidentialite(): Response
+    {
+        return $this->render('frontend/pages/politique_de_confidentialite.html.twig');
+    }
+
+    #[Route('/jeux/{slugGame}', name: 'view_game')]
+    public function viewGame($slugGame): Response
+    {
+        return $this->render('frontend/pages/game.html.twig');
+    }
+
+    #[Route('/search-product', name: 'search_product')]
+    public function searcProduct(): Response
+    {
+        return $this->render('frontend/pages/search.html.twig');
     }
 }
