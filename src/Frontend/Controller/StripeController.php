7e912f7e02b9d310e9ac3ba75e5b09c74daca282
Nicolas GRITTE <nicolas.gritte@hotmail.fr>
2023-09-08 17:14:13 +0200

    step1

--- src/Frontend/Controller/StripeController.php
+++ src/Frontend/Controller/StripeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Frontend\Controller;
 
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -41,17 +44,22 @@ class StripeController extends AbstractController
  
  
     #[Route('/payement-is-succes', name: 'app_stripe_charge')]
-    public function createCharge(SessionInterface $session, MailerInterface $mailer)
+    public function createCharge(SessionInterface $session, MailerInterface $mailer, OrderRepository $orderRepository, EntityManagerInterface $em)
     {
         $this->addFlash(
             'success',
             'Le payement a bien était éffectué'
         );
+        $order =  $orderRepository->findOneBy(['step' => 0, 'user' => $this->getUser()]);
+        $order->setStep(1);
+        $em->persist($order);
+        $em->flush($order);
+
         $email = (new Email())
         ->from('escromania3993@outlook.fr')
         ->to('escromania3993@outlook.fr')
-        ->subject('Demande de contact')
-        ->html('<p>De : Escromania </p><p>Email: escromania3993@outlook.fr </p><p>Message : <br><br> Merci pour votre commande </p><p>Ce message provient du site <a href="http://127.0.0.1:8000">Escromania</a></p>');
+        ->subject('Confirmation de Paiement')
+        ->html('<p>De : Escromania </p><p>Email: escromania3993@outlook.fr </p><p>Message : <br><br> Merci pour ta commande <br>Tkt on te livre bientôt</p><p>Ce message provient du site <a href="http://127.0.0.1:8000">Escromania</a></p>');
 
         $mailer->send($email);
         $session->set('panier', []);
